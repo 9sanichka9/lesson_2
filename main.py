91a09487156e5a80b8c547fc51118aea8bf93362
Home <varoda99@gmail.com>
2024-10-22 21:22:25 +0300

    Додано порожній файл script.py Додано код для виведення імені, віку та спічу

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+name = "Саша"
+age = 12
+speech = "Я навчаюся у IT Step, тому що хочу освоїти програмування. Це відкриває багато можливостей у кар'єрі. Мені цікаво створювати ігри та програми."
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(name)
+print(age)
+print(speech)
